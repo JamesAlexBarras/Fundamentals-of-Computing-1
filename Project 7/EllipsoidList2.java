b25a9aa22f1ba654df6740f5901e34be5eb72e3f
JamesAlexBarras <80291385+JamesAlexBarras@users.noreply.github.com>
2021-03-10 21:06:23 -0600

    Add files via upload

---
+++ Project 7/EllipsoidList2.java
@@ -0,0 +1,323 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+/**
+Provides the methods for the EllipsoidListApp.
+
+Project 07
+ @author James Barras comp 1210 LLB 010
+ @version March 4th 2020
+*/
+public class EllipsoidList2 {
+   private String ellipListname;
+   private int listsize;
+   private Ellipsoid[] ellipsoidList;
+   //private ArrayList<Ellipsoid> ellipList;
+
+/**
+Creates the Ellipsoid list from a string and an array.
+
+@param ellipLiname is the name of the list
+@param ellipListIn is the array.
+@param listsizeIn is the inputted listsize
+*/
+   public EllipsoidList2(String ellipLiname, Ellipsoid[] ellipListIn, 
+         int listsizeIn) {
+      ellipListname = ellipLiname;
+      listsize = listsizeIn;
+      ellipsoidList = ellipListIn;
+      //ellipList = ellipListIn;
+   
+   }
+
+/**
+returns the name.
+
+@return String
+*/
+   public String getName() {
+      return ellipListname;
+   }
+   
+  /**
+  returns the number of ellipsoids as an int.
+  
+  @return int
+  */
+   public int numberOfEllipsoids() {
+      int none = 0;
+      if (listsize > none) {
+         return listsize;
+      }
+      else {
+         return none;
+      }
+   }
+
+
+/**
+calculates and returns the total volume.
+
+@return double
+*/
+   public double totalVolume() {
+      int objects = 0;
+      double total = 0.0;
+      
+      if (listsize == objects) {
+         return total;
+      }
+      while (objects < listsize) {
+         total += ellipsoidList[objects].volume();
+         objects++;
+      }
+      return total;
+   }
+   
+ /**
+ calculates and returns the total surface area.
+ 
+ @return double
+ */  
+   public double totalSurfaceArea() {
+      int objects = 0;
+      double total = 0.0;
+   
+      if (listsize == objects) {
+         return total;
+      }
+      while (objects < listsize) {
+         total += ellipsoidList[objects].surfaceArea();
+         objects++;
+      }
+      return total;
+   }
+
+/**
+calculates and returns average volume.
+
+@return double
+*/
+   public double averageVolume() {
+      int objects = 0;
+      double total = 0.0;
+      double average = 0.0;
+   
+      if (listsize == objects) {
+         return total;
+      }
+      while (objects < listsize) {
+         total += ellipsoidList[objects].volume();
+         objects++;
+      }
+      average = (total / listsize);
+      return average;
+   }
+   
+  /**
+  calculates and returns average surface area.
+  
+  @return double
+  */
+   public double averageSurfaceArea() {
+      int objects = 0;
+      double total = 0.0;
+      double average = 0.0;
+   
+      if (listsize == objects) {
+         return total;
+      }
+      while (objects < listsize) {
+         total += ellipsoidList[objects].surfaceArea();
+         objects++;
+      }
+      average = (total / listsize);
+      return average;
+   }
+ 
+ /**
+ converts the array into a string display.
+ 
+ @return String
+ */ 
+   public String toString() {
+      int objects = 0;
+   
+      String totalstring = ellipListname + "\n";
+      while (objects < listsize) {
+         totalstring += "\n" + ellipsoidList[objects] + "\n";
+         objects++;
+      }
+      return totalstring;
+   }
+   
+  /**
+  reuturns a summary of the array's information.
+  
+  @return String
+  */ 
+   public String summaryInfo() {
+      DecimalFormat dfformat = new DecimalFormat("#,##0.0##");
+      String summary = "";
+      
+      summary += "----- Summary for " + ellipListname + " -----"
+         + "\nNumber of Ellipsoid Objects: " + numberOfEllipsoids()
+         + "\nTotal Volume: " + dfformat.format(totalVolume()) + " cubic units"
+         + "\nTotal Surface Area: " + dfformat.format(totalSurfaceArea()) 
+         + " square units"
+         + "\nAverage Volume: " + dfformat.format(averageVolume()) 
+         + " cubic units"
+         + "\nAverage Surface Area: " + dfformat.format(averageSurfaceArea()) 
+         + " square units"
+         + "\n";
+   
+      return summary;
+   }
+   
+   
+   ///////////////////////////////
+   ///////////////////////////////
+   
+   /**
+   gives array of the inputted list.
+   
+   @return ArrayList<Ellipsoid>
+   */
+   public Ellipsoid[] getList() {
+      return ellipsoidList;
+   }
+   
+   /**
+   takes an inputted file name and searches for the file.
+   then takes the information from the file and 
+   creates a list.
+   
+   @return EllipsoidList
+   @param filenamein is the file name given by the user
+   @throws FileNotFoundException if the file cannot be opened.
+
+   */
+   public EllipsoidList2 readFile(String filenamein)
+               throws FileNotFoundException {
+     
+      String listname = "";
+      Ellipsoid[] arrayfromfile = new Ellipsoid[100];
+      Scanner filescan = new Scanner(new File(filenamein));
+      int numofEllipsoids = 0;
+      
+      listname = filescan.nextLine();
+      while (filescan.hasNext()) {
+         
+         String label = filescan.nextLine();
+         Double axis1 = Double.parseDouble(filescan.nextLine());
+         Double axis2 = Double.parseDouble(filescan.nextLine());
+         Double axis3 = Double.parseDouble(filescan.nextLine()); 
+      
+      
+         Ellipsoid newellip = new Ellipsoid(label, axis1, axis2, axis3);
+         arrayfromfile[numofEllipsoids] = newellip;
+         numofEllipsoids++;
+      }
+      EllipsoidList2 listfromfile = new EllipsoidList2(listname, arrayfromfile, 
+            numofEllipsoids);
+      
+      return listfromfile;
+   }
+   
+   /**
+  Takes inputs needed to create an ellipsoid.
+  then adds that ellipsoid to the list.
+  
+  @param label is the inputted label
+   @param a is the inputted a axis
+   @param b is the inputted b axis
+   @param c is the inputted c axis
+   */
+   public void addEllipsoid(String label, double a, double b, double c) {
+      Ellipsoid ellipnew = new Ellipsoid(label, a, b, c);
+      ellipsoidList[listsize] = ellipnew;
+      listsize++;
+   }
+  
+  /**
+  finds a label in an array based on the inputted label.
+  
+  @return Ellipsoid
+  @param label1 is the inputted label
+  */ 
+   public Ellipsoid findEllipsoid(String label1) {
+      Ellipsoid e = null;
+      
+      for (int i = 0; i < listsize; i++) {
+         if (label1.compareToIgnoreCase(ellipsoidList[i].getLabel()) == 0) {
+            e = ellipsoidList[i];
+            break;
+         }
+      }
+      return e;
+   }
+   
+   /**
+   deletes an ellipsoid in an array based.
+   on the inputted label.
+  
+  @param label3 is the inputted label
+  @return Ellipsoid 
+   */
+   public Ellipsoid deleteEllipsoid(String label3) {
+      Ellipsoid theone = null;
+      
+      for (int i = 0; i < listsize; i++) {
+         
+         if (ellipsoidList[i].getLabel().compareToIgnoreCase(label3) == 0) {
+            theone = ellipsoidList[i];
+            
+            for (int j = i; j < listsize - 1; j++) {
+               ellipsoidList[j] = ellipsoidList[j + 1];
+            }
+            ellipsoidList[listsize - 1] = null;
+            listsize--;
+          
+            return theone;
+         }
+      }
+      return theone;
+      /**
+      if (findEllipsoid(label3) != null) { 
+         theone = findEllipsoid(label3);
+         
+         ellipList.remove(findEllipsoid(label3));
+         return theone;
+      }
+      else {
+         return theone;
+      }
+      */
+   }
+   
+   /**
+   edits an ellipsoid of an inputted label.
+   
+   @return Ellipsoid
+   @param label is the inputted label
+   @param a is the inputted a axis
+   @param b is the inputted b axis
+   @param c is the inputted c axis
+   */
+   public Ellipsoid editEllipsoid(String label, double a, double b, double c) {
+      Ellipsoid edited = null;
+      if (findEllipsoid(label) != null) {
+         edited = findEllipsoid(label);
+         
+         edited.setA(a);
+         edited.setB(b);
+         edited.setC(c);
+         
+         return edited;
+      }
+      else {
+         return edited;
+      }
+   }
+}
---
+++ Project 7/EllipsoidList2MenuApp.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+/**
+Uses methods from ellipsoidlist and ellipsoid.
+in order to execute various menu options. 
+
+Project 07
+ @author James Barras comp 1210 LLB 010
+ @version March 4th 2020
+*/
+public class EllipsoidList2MenuApp {
+/**
+is the main method that gives the user an input.
+and then executes the various menu choices.
+
+@param args - is not used.
+@throws FileNotFoundException if the file cannot be opened.
+*/
+   public static void main(String[] args) throws FileNotFoundException {
+      Scanner userInput = new Scanner(System.in);
+      String input = "";
+      Ellipsoid[] fullarray = new Ellipsoid[100];
+      EllipsoidList2 fulllist = new EllipsoidList2(input, fullarray, 0);
+   
+   
+      System.out.print("Ellipsoid List System Menu"
+         + "\nR - Read File and Create Ellipsoid List"
+         + "\nP - Print Ellipsoid List"
+         + "\nS - Print Summary"
+         + "\nA - Add Ellipsoid"
+         + "\nD - Delete Ellipsoid"
+         + "\nF - Find Ellipsoid"
+         + "\nE - Edit Ellipsoid"
+         + "\nQ - Quit");
+     
+      do {
+         System.out.print("\nEnter Code [R, P, S, A, D, F, E, or Q]: ");
+         input = userInput.nextLine();
+         
+         if (input.length() == 0) {
+            continue;
+         }
+         input = input.toUpperCase();
+         char menuchoice = input.charAt(0);
+         
+         switch (menuchoice) {
+            case 'R': 
+               System.out.print("\tFile Name: ");
+               String filename = userInput.nextLine();
+               
+               fulllist = fulllist.readFile(filename);
+               
+               System.out.println("\tFile read in "
+                  + "and Ellipsoid List created");
+               break;
+               
+            case 'P':
+               System.out.print(fulllist);
+               break;
+            
+            case 'S':
+               System.out.print("\n" + fulllist.summaryInfo());
+               break;
+            
+            case 'A':
+               System.out.print("\tlabel: ");
+               String label1 = userInput.nextLine();
+               System.out.print("\ta: ");
+               double axisa = Double.parseDouble(userInput.nextLine());
+               System.out.print("\tb: ");
+               double axisb = Double.parseDouble(userInput.nextLine());
+               System.out.print("\tc: ");
+               double axisc = Double.parseDouble(userInput.nextLine());
+            
+               fulllist.addEllipsoid(label1, axisa, axisb, axisc);
+               System.out.print("\t*** Ellipsoid added ***" + "\n");
+            
+               break;
+               
+            case 'D':
+               System.out.print("\tlabel: ");
+               String label = userInput.nextLine();
+               
+               if (fulllist.findEllipsoid(label) != null) {
+                  Ellipsoid saved = fulllist.findEllipsoid(label);
+                  String savedlabel = saved.getLabel();
+               
+                  fulllist.deleteEllipsoid(label);
+                  System.out.println("\t\"" + savedlabel + "\" deleted");
+               } 
+               else {
+                  System.out.println("\t\"" + label + "\" not found");
+                  break;
+               }
+               break;
+            
+            case 'F':
+               System.out.print("\tlabel: ");
+               String label45 = userInput.nextLine();
+               if (fulllist.findEllipsoid(label45) == null) {
+                  System.out.println("\t\"" + label45 + "\" not found");
+               }
+               else {
+                  Ellipsoid foundellip = fulllist.findEllipsoid(label45);
+                  String printed = foundellip.toString();
+               
+                  System.out.print(printed + "\n");
+               }
+               break;
+            
+            case 'E':
+               System.out.print("\tlabel: ");
+               String labelf = userInput.nextLine();
+               System.out.print("\ta: ");
+               double axisaf = Double.parseDouble(userInput.nextLine());
+               System.out.print("\tb: ");
+               double axisbf = Double.parseDouble(userInput.nextLine());
+               System.out.print("\tc: ");
+               double axiscf = Double.parseDouble(userInput.nextLine());
+            
+               if (fulllist.editEllipsoid(labelf, axisaf, axisbf, axiscf)
+                  == null) {
+                  System.out.println("\t\"" + labelf + "\" not found");
+               }
+               else {
+                  Ellipsoid foundellip = fulllist.findEllipsoid(labelf);
+                  String saved = foundellip.getLabel();
+               
+                  fulllist.editEllipsoid(labelf, axisaf, axisbf, axiscf);
+               
+                  System.out.println("\t\"" + saved + "\" successfully edited");
+               }
+            
+            case 'Q':
+               break;
+            default:
+               System.out.print("\t*** invalid code ***" + "\n");
+         }
+      } while (!input.equalsIgnoreCase("Q"));
+   }
+}
